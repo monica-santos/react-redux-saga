5c22e6b739c4d24e5a23b634b2bc6ea205aa52a5
Mônica Santos <monicasantosdev@gmail.com>
2020-02-23 12:45:00 -0300

    styled home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,160 @@
 import React from 'react'
+import { MdAddShoppingCart } from 'react-icons/md'
+import { ProducList } from './styles'
 
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <ProducList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-pace-masculino/06/COL-0552-006/COL-0552-006_zoom1.jpg?ims=544x"
+          alt="Tênis Adidas Vs Pace"
+        />
+
+        <strong>Tênis Adidas Vs Pace</strong>
+        <span>R$169,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-pace-masculino/06/COL-0552-006/COL-0552-006_zoom1.jpg?ims=544x"
+          alt="Tênis Adidas Vs Pace"
+        />
+
+        <strong>Tênis Adidas Vs Pace</strong>
+        <span>R$169,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-pace-masculino/06/COL-0552-006/COL-0552-006_zoom1.jpg?ims=544x"
+          alt="Tênis Adidas Vs Pace"
+        />
+
+        <strong>Tênis Adidas Vs Pace</strong>
+        <span>R$169,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-pace-masculino/06/COL-0552-006/COL-0552-006_zoom1.jpg?ims=544x"
+          alt="Tênis Adidas Vs Pace"
+        />
+
+        <strong>Tênis Adidas Vs Pace</strong>
+        <span>R$169,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-pace-masculino/06/COL-0552-006/COL-0552-006_zoom1.jpg?ims=544x"
+          alt="Tênis Adidas Vs Pace"
+        />
+
+        <strong>Tênis Adidas Vs Pace</strong>
+        <span>R$169,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-pace-masculino/06/COL-0552-006/COL-0552-006_zoom1.jpg?ims=544x"
+          alt="Tênis Adidas Vs Pace"
+        />
+
+        <strong>Tênis Adidas Vs Pace</strong>
+        <span>R$169,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-pace-masculino/06/COL-0552-006/COL-0552-006_zoom1.jpg?ims=544x"
+          alt="Tênis Adidas Vs Pace"
+        />
+
+        <strong>Tênis Adidas Vs Pace</strong>
+        <span>R$169,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-pace-masculino/06/COL-0552-006/COL-0552-006_zoom1.jpg?ims=544x"
+          alt="Tênis Adidas Vs Pace"
+        />
+
+        <strong>Tênis Adidas Vs Pace</strong>
+        <span>R$169,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-pace-masculino/06/COL-0552-006/COL-0552-006_zoom1.jpg?ims=544x"
+          alt="Tênis Adidas Vs Pace"
+        />
+
+        <strong>Tênis Adidas Vs Pace</strong>
+        <span>R$169,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-vs-pace-masculino/06/COL-0552-006/COL-0552-006_zoom1.jpg?ims=544x"
+          alt="Tênis Adidas Vs Pace"
+        />
+
+        <strong>Tênis Adidas Vs Pace</strong>
+        <span>R$169,99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProducList>
+  )
 }
